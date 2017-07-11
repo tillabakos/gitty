513623b9752cefa0f58adcdc456438d94851e9bf
Kurucz Péter <k.fantoser@gmail.com>
2017-07-11 09:58:10 +0200

    Implemented joke() function

--- gittie.py
+++ gittie.py
@@ -1,2 +1,10 @@
+import random
+
 def introduce():
     print("Hello, I'm Gittie!")
+
+def joke():
+    jokes = [["Why was the stadium so cold?", "Because there were a lot of fans."], ["What did one plate say to the other?", "Lunch is on me."], ["Where do animals go when their tails fall off?", "The retail store."], ["What do you call a sad cup of coffee?", "Depresso"]]
+    joke = random.choice(jokes)
+    input(joke[0])
+    print(joke[1])
